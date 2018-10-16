be698857ca6fdcf3b6e2bc5d7e0af7287d7dc4c5
AnaghaJoshi111 <anaghadeepaj@yahoo.com>
2018-10-16 13:22:07 +0530

    First commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+// import for observales
+import { Observable, Observer, Subscription } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy{
+  numberObservableSubscription : Subscription;
+  customObservableSubscription : Subscription;
 
   constructor() { }
 
   ngOnInit() {
+
+    // observable
+    // understanding observable interval method working
+    // built in observable function
+      const myNumbers = Observable.interval(1000)
+                          .map(
+                            (data : number ) => {
+                                return data * 2;
+                            }
+                          );
+      this.numberObservableSubscription = myNumbers.subscribe(
+        (number : number ) => {
+          console.log(number);
+      });
+    
+      // user defined Observable creation
+    const myObservable = Observable.create(
+      (observer : Observer<string>) => {
+          setTimeout(() => {
+            observer.next('first package');
+          },2000);
+          setTimeout(() => {
+            observer.next('second package');
+          },4000);
+          setTimeout(() => {
+            //observer.error('this does not work ...!!!');
+
+            observer.complete();
+          },5000);
+          setTimeout(() => {
+            observer.next('third package');
+          },6000);
+      });
+      this.customObservableSubscription = myObservable.subscribe(
+        ( data : string ) => {
+          console.log(data);
+        },
+        ( error : string ) => {
+          console.log(error);
+        },
+        () => {
+          console.log('completed...!!!');
+        }
+      );
   }
 
+
+  ngOnDestroy(){
+    this.numberObservableSubscription.unsubscribe();
+    this.customObservableSubscription.unsubscribe();
+  }
 }
